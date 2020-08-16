36bfa07c1aadfd2ab5a1ce048cb4d7921d43b351
shcampos <samuel-hc@hotmail.com>
2020-08-16 15:51:25 -0400

    feat: add card and responsivity

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Card, CardActions, CardContent, Button } from '@material-ui/core';
+import useWindowDimensions from './assets/scripts/useWindowDimensions.js';
+
 function App() {
+  const { height, width } = useWindowDimensions();
+  //const height = window.innerHeight();
+  const styles = makeStyles({
+    root: {
+      padding: '0px',
+      margin: '0px',
+      width: width,
+      height: height,
+      backgroundColor: '#0A1041'
+    },
+    card: {
+      backgroundColor: '#79CFD9'
+    }
+  });
+  const classes = styles();
+  const [isReady, setIsReady] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {isReady ? (
+        <Grid  container spacing={0} alignItems="center" justify="center"
+        style={{ minHeight: height }}>
+          <Grid item xs={3}>
+            <Card className={classes.card}>
+              <CardContent>
+              <h2>MycoGame</h2>
+              Um jeito diferente de ir a campo para coletar macrofungos e identificá-los!<br/>
+              Vamos nessa!
+              </CardContent>
+              <CardActions>
+                <Button className={classes.button} onClick={() => setIsReady(false)}>COMEÇAR</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      ) : (
+        <p>asbashio</p>
+      )}
     </div>
   );
 }
